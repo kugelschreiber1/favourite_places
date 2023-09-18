3662af497fcb1d65f55f145399033aaa1036cace
Trevor Toni <badrobot.f4@gmail.com>
2023-09-18 10:16:31 +0300

    Added background image

--- lib/screens/place_details_screen.dart
+++ lib/screens/place_details_screen.dart
@@ -1,19 +1,28 @@
+import 'package:favourite_places/models/place_model.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class PlaceDetailsScreen extends StatelessWidget {
-  const PlaceDetailsScreen({required this.title, super.key});
+class PlaceDetailsScreen extends ConsumerWidget {
+  const PlaceDetailsScreen({required this.place, super.key});
 
-  final String title;
+  final Place place;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: Text(title),
+        title: Text(place.title),
       ),
-      body: const Center(
-        child: Text("Place Details"),
+      body: Stack(
+        children: [
+          Image.file(
+            place.image,
+            fit: BoxFit.cover,
+            width: double.infinity,
+            height: double.infinity,
+          ),
+        ],
       ),
     );
   }
